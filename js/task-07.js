d3973b58b22a52a1d623df0328e7cd40a4c07033
Makiz7791 <topstar77@ukr.net>
2021-09-29 10:55:55 +0300

    goit-js-hw-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,18 +1,6 @@
 const inputRef = document.querySelector('#font-size-control');
-const textRef = document.querySelector('#text');
+const magicSpanTextRef = document.querySelector('#text');
 
-const rangeInputsetUp = (minRem = 1, maxRem = 5, step = 1) => {
-  inputRef.value = minRem;
-  textRef.style.fontSize = inputRef.value + 'rem';
-  inputRef.min = minRem;
-  inputRef.max = maxRem;
-  inputRef.step = step;
-};
-
-rangeInputsetUp();
-
-inputRef.addEventListener('change', onInputChange);
-
-function onInputChange(e) {
-  textRef.style.fontSize = e.target.value + 'rem';
-}
+inputRef.addEventListener('input', event => {
+  magicSpanTextRef.style.fontSize = event.currentTarget.value + 'px';
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,37 +1,68 @@
-const boxesRef = document.querySelector('#boxes');
-const inputRef = document.querySelector('div input');
-const controlsRef = document.querySelector('#controls');
-const createBoxesBtnRef = document.querySelector('[data-action="render"]');
-const destroyBoxesBtnRef = document.querySelector('[data-action="destroy"]');
-
-controlsRef.style.marginBottom = 10 + 'px';
-boxesRef.style.display = 'flex';
-
-console.log(boxesRef);
-
-const createBoxes = amount => {
-  if (amount > parseInt(inputRef.getAttribute('max')))
-    return console.log(`Enter a number <= ${inputRef.getAttribute('max')}`);
-  if (amount < parseInt(inputRef.getAttribute('min')))
-    return console.log(`Enter a number >= ${inputRef.getAttribute('min')}`);
-
-  let boxes = '';
-  for (let i = 1; i <= amount; i++) {
-    boxes += `<div class='box-${i}' style='width: ${i * 10 + 20}px; height: ${
-      i * 10 + 20
-    }px; background-color: #${Math.floor(Math.random() * 16777215).toString(
-      16,
-    )}; margin-right: 10px; border: solid;'></div>`;
-  }
-  return boxesRef.insertAdjacentHTML('beforeend', boxes);
+const refs = {
+  input: document.querySelector('input[type="number"]'),
+  renderBtn: document.querySelector('button[data-action="render"]'),
+  destroyBtn: document.querySelector('button[data-action="destroy"]'),
+  container: document.querySelector('#boxes'),
 };
 
-createBoxesBtnRef.addEventListener('click', () => {
-  boxesRef.innerHTML = '';
-  createBoxes(inputRef.value);
-});
+refs.renderBtn.textContent += ' (Ctrl + Enter)';
+refs.destroyBtn.textContent += ' (Esc)';
+refs.input.value = Math.ceil(Math.random() * 20);
 
-destroyBoxesBtnRef.addEventListener('click', () => {
-  boxesRef.innerHTML = '';
-  inputRef.value = 0;
-});
+const randomizeInputValue = () =>
+  (refs.input.value = Math.ceil(Math.random() * 20));
+
+// Functions to create random RGB color
+
+const generateRandomRgbValue = () => Math.round(255 * Math.random());
+
+const generateRandomRgbColor = () =>
+  `rgb(${generateRandomRgbValue()}, ${generateRandomRgbValue()}, ${generateRandomRgbValue()})`;
+
+// Function to create array with length that is taken from input
+
+const createIterableArray = length => new Array(length).fill(0);
+
+// Function to create one div box element including task conditions
+
+const createDivEl = (divEl, currentIndex) => {
+  divEl = document.createElement('div');
+  divEl.textContent = 1 + currentIndex;
+  divEl.style.width = 30 + currentIndex * 10 + 'px';
+  divEl.style.height = 30 + currentIndex * 10 + 'px';
+  divEl.style.background = generateRandomRgbColor();
+  divEl.style.margin = '5px';
+  return divEl;
+};
+
+// add listeners
+
+refs.renderBtn.addEventListener('click', onRenderBtnClick);
+
+function onRenderBtnClick() {
+  const boxQty = Number(refs.input.value);
+  const iterableArray = createIterableArray(boxQty);
+  const divElements = iterableArray.map(createDivEl);
+  refs.container.append(...divElements);
+}
+
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  refs.container.innerHTML = '';
+  randomizeInputValue();
+}
+
+window.addEventListener('keydown', onEnterPress);
+
+function onEnterPress(e) {
+  if (
+    (e.code === 'Enter' && e.ctrlKey) ||
+    (e.code === 'NumpadEnter' && e.ctrlKey)
+  ) {
+    onRenderBtnClick();
+  }
+  if (e.code === 'Escape') {
+    destroyBoxes();
+  }
+}
